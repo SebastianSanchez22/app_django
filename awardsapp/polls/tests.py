8353c837f41711958c91436f8c350c2d6cefd9ec
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-01-08 18:03:11 -0500

    Add test no questions

--- awardsapp/polls/tests.py
+++ awardsapp/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls.base import reverse
 from django.utils import timezone
 
 from .models import Question
@@ -18,12 +19,19 @@ class QuestionModelTests(TestCase):
         """was_published_recently() must return True for questions whose pub_date is actual"""
         time = timezone.now()
         present_question = Question(question_text="¿Quien es el mejor CD de Platzi?",pub_date=time)
-        self.assertIs(present_question.was_published_recently(),True)
+        self.assertIs(present_question.was_published_recently(), True)
 
     def test_was_published_recently_with_past_questions(self):
         """was_published_recently() must return Flase for questions whose pub_date is more than 1 day in the past"""
         time = timezone.now() - datetime.timedelta(days=30)
         past_question = Question(question_text="¿Quien es el mejor CD de Platzi?",pub_date=time)
-        self.assertIs(past_question.was_published_recently(),False)
+        self.assertIs(past_question.was_published_recently(), False)
 
-    
+class QuestionIndexViewTest(TestCase):
+
+    def test_no_questions(self):
+        """If no question exists, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
+from django.utils import timezone
+
 from .models import Question, Choice
 
 
@@ -32,7 +34,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the five last published questions"""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte =timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
