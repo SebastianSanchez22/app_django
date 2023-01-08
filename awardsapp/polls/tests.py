443ec39ef59c87c12dacccf044865084d63d35dc
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-01-08 18:53:40 -0500

    Checking tests for different scenarios in index

--- awardsapp/polls/tests.py
+++ awardsapp/polls/tests.py
@@ -17,21 +17,20 @@ def create_question(question_text, days):
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_questions(self):
-        """was_published_recently returns False for questions whose pud_date is in the future"""
+        """was_published_recently must return False for questions whose pub_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="¿Cual es el mejor CD de Platzi?", pub_date=time)
+        future_question = create_question("Future question", days=30)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_present_questions(self):
         """was_published_recently() must return True for questions whose pub_date is actual"""
-        time = timezone.now()
-        present_question = Question(question_text="¿Quien es el mejor CD de Platzi?",pub_date=time)
+        present_question = create_question("Present question", days=0)
         self.assertIs(present_question.was_published_recently(), True)
 
     def test_was_published_recently_with_past_questions(self):
-        """was_published_recently() must return Flase for questions whose pub_date is more than 1 day in the past"""
-        question = create_question("Future question", days=30)
-        self.assertIs(question.was_published_recently(), False)
+        """was_published_recently() must return False for questions whose pub_date is more than 1 day in the past"""
+        past_question = create_question("Past question", days=-30)
+        self.assertIs(past_question.was_published_recently(), False)
 
 class QuestionIndexViewTest(TestCase):
 
@@ -55,6 +54,24 @@ class QuestionIndexViewTest(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"], [question])
 
-    
-        
+    def test_future_question_and_past_question(self):
+        """Even if both passed and future questions exists, only past questions are displayed"""
+        past_question = create_question(question_text="Past question", days=-30)
+        future_question = create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [past_question])
+
+    def test_two_past_questions(self):
+        """The questions index page may display multiple questions"""
+        past_question1 = create_question(question_text="Past question 1", days=-30)
+        past_question2 = create_question(question_text="Past question 2", days=-40)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [past_question1, past_question2])
+
+    def test_two_future_questions(self):
+        """Future questions may not display even if there are many"""
+        future_question1 = create_question(question_text="Future question 1", days=30)
+        future_question2 = create_question(question_text="Future question 2", days=40)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
         
