79a731493e3a387334b9a96355031baf30d77502
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-01-07 23:48:42 -0500

    Finished form

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -8,14 +10,31 @@ def index(request):
         "latest_question_list": latest_question_list
     })
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {
         "question": question
     })
 
+
 def results(request, question_id):
-    return HttpResponse(f"Resultados de la pregunta N°{question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
+
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando por la pregunta N°{question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
