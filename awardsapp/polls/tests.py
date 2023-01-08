03e3a788aa31d4261439031f68ca11d7323eaac1
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-01-08 15:40:56 -0500

    Adding tests for function was_published_recently in questions

--- awardsapp/polls/tests.py
+++ awardsapp/polls/tests.py
@@ -9,7 +9,21 @@ from .models import Question
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_questions(self):
-        """was_published_recently returns False for questions whose pub_date is in the future"""
+        """was_published_recently returns False for questions whose pud_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="¿Quién es el mejor CD de Platzi?", pub_date=time)
+        future_question = Question(question_text="¿Cual es el mejor CD de Platzi?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        """was_published_recently() must return True for questions whose pub_date is actual"""
+        time = timezone.now()
+        present_question = Question(question_text="¿Quien es el mejor CD de Platzi?",pub_date=time)
+        self.assertIs(present_question.was_published_recently(),True)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently() must return Flase for questions whose pub_date is more than 1 day in the past"""
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text="¿Quien es el mejor CD de Platzi?",pub_date=time)
+        self.assertIs(past_question.was_published_recently(),False)
+
+    
